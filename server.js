6a2375a87ad49f26d110f4a52910582448da23f3
Sung Chan Yang <yang@Sungs-MacBook-Pro.local>
2018-12-05 18:02:01 -0500

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000
 
 
 var app = express();
@@ -52,6 +53,6 @@ app.get('/bad', (req,res)=>{
         errorMessage: "UNABLE TO GET USER REQUEST"
     })
 })
-app.listen(3000, ()=>{
-    console.log('Server 3000 is running---');
+app.listen(port, ()=>{
+    console.log(`Server ${port} is running!`);
 });
